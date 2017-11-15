c19c0635e7ac72903bda48f53848ec925400fa80
Emilio Coronado <emilio.mobile@gmail.com>
2017-11-15 18:25:29 +0900

    Fixing naming

--- src/app/shared/pipes/ObjectArrayType.pipe.ts
+++ src/app/shared/pipes/ObjectArrayType.pipe.ts
@@ -11,6 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+// Detecting if an object is an array
 import { PipeTransform, Pipe } from '@angular/core';
 
 @Pipe({name: 'isObjectArray'})
